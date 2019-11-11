f366acfa9eaf2e53f22672a18b28863388067825
Utkarsh Bhimte <utkarshbhimte95@gmail.com>
2019-11-12 00:01:08 +0530

    feat: added the custom placeholder props

--- src/index.js
+++ src/index.js
@@ -1,114 +1,156 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 // fake data generator
 const getItems = count =>
-  Array.from({ length: count }, (v, k) => k).map(k => ({
-    id: `item-${k}`,
-    content: `item ${k}`
-  }));
+	Array.from({ length: count }, (v, k) => k).map(k => ({
+		id: `item-${k}`,
+		content: `item ${k}`
+	}));
 
 // a little function to help us with reordering the result
 const reorder = (list, startIndex, endIndex) => {
-  const result = Array.from(list);
-  const [removed] = result.splice(startIndex, 1);
-  result.splice(endIndex, 0, removed);
+	const result = Array.from(list);
+	const [removed] = result.splice(startIndex, 1);
+	result.splice(endIndex, 0, removed);
 
-  return result;
+	return result;
 };
 
 const grid = 8;
 
 const getItemStyle = (isDragging, draggableStyle) => ({
-  // some basic styles to make the items look a bit nicer
-  userSelect: "none",
-  padding: grid * 2,
-  margin: `0 0 ${grid}px 0`,
+	// some basic styles to make the items look a bit nicer
+	userSelect: "none",
+	padding: grid * 2,
+	margin: `0 0 ${grid}px 0`,
 
-  // change background colour if dragging
-  background: isDragging ? "lightgreen" : "grey",
+	// change background colour if dragging
+	background: isDragging ? "lightgreen" : "grey",
 
-  // styles we need to apply on draggables
-  ...draggableStyle
+	// styles we need to apply on draggables
+	...draggableStyle
 });
 
 const getListStyle = isDraggingOver => ({
-  background: isDraggingOver ? "lightblue" : "lightgrey",
-  padding: grid,
-  width: 250
+	background: isDraggingOver ? "lightblue" : "lightgrey",
+	padding: grid,
+  width: 250,
+  position: "relative"
 });
 
+const queryAttr = "data-rbd-drag-handle-draggable-id";
 const CustomPlaceholder = ({ snapshot }) => {
-  return <div>current: {snapshot.draggingOverWith}</div>;
+	const draggedDOM = React.useMemo(() => {
+		const domQuery = `[${queryAttr}='${snapshot.draggingOverWith}']`;
+		return document.querySelector(domQuery);
+	}, [snapshot.draggingOverWith]);
+
+	React.useEffect(() => {
+		if (!!draggedDOM) {
+			const parent = draggedDOM.parentNode;
+			// The equivalent of parent.children.indexOf(child)
+			var index = Array.prototype.indexOf.call(parent.children, draggedDOM);
+			console.log("TCL: CustomPlaceholder -> index", index);
+		}
+	}, [draggedDOM]);
+
+	console.log("TCL: CustomPlaceholder -> draggedDOM", draggedDOM);
+
+	return <div>current: {snapshot && snapshot.draggingOverWith}</div>;
 };
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: getItems(10)
-    };
-    this.onDragEnd = this.onDragEnd.bind(this);
-  }
+const App = props => {
+	const [placeholderProps, setPlaceholderProps] = useState({});
+	const [items, setItems] = useState(getItems(10));
 
-  onDragEnd(result) {
-    // dropped outside the list
-    if (!result.destination) {
+	const onDragEnd = result => {
+		// dropped outside the list
+		if (!result.destination) {
+			return;
+		}
+
+    setPlaceholderProps({})
+		setItems(items => reorder(items, result.source.index, result.destination.index));
+	};
+
+	const onDragUpdate = update => {
+    if(!update.destination){
       return;
     }
+		const draggableId = update.draggableId;
+		const destinationIndex = update.destination.index;
 
-    const items = reorder(
-      this.state.items,
-      result.source.index,
-      result.destination.index
-    );
+		const domQuery = `[${queryAttr}='${draggableId}']`;
+		const draggedDOM = document.querySelector(domQuery);
 
-    this.setState({
-      items
-    });
-  }
+		if (!draggedDOM) {
+			return;
+		}
+		const { clientHeight, clientWidth } = draggedDOM;
 
-  // Normally you would want to split things out into separate components.
-  // But in this example everything is just done in one place for simplicity
-  render() {
-    return (
-      <DragDropContext onDragEnd={this.onDragEnd}>
-        <Droppable droppableId="droppable">
-          {(provided, snapshot) => (
-            <div
-              {...provided.droppableProps}
-              ref={provided.innerRef}
-              style={getListStyle(snapshot.isDraggingOver)}
-            >
-              {console.log({ snapshot })}
-              {this.state.items.map((item, index) => (
-                <Draggable key={item.id} draggableId={item.id} index={index}>
-                  {(provided, snapshot) => (
-                    <div
-                      ref={provided.innerRef}
-                      {...provided.draggableProps}
-                      {...provided.dragHandleProps}
-                      style={getItemStyle(
-                        snapshot.isDragging,
-                        provided.draggableProps.style
-                      )}
-                    >
-                      {item.content}
-                    </div>
-                  )}
-                </Draggable>
-              ))}
+		const clientY = parseFloat(window.getComputedStyle(draggedDOM.parentNode).paddingTop) + [...draggedDOM.parentNode.children]
+			.slice(0, destinationIndex)
+			.reduce((total, curr) => {
+				const style = curr.currentStyle || window.getComputedStyle(curr);
+				const marginBottom = parseFloat(style.marginBottom);
+				return total + curr.clientHeight + marginBottom;
+			}, 0);
 
-              {provided.placeholder}
-              <CustomPlaceholder snapshot={provided.snapshot} />
-            </div>
-          )}
-        </Droppable>
-      </DragDropContext>
-    );
-  }
-}
+		setPlaceholderProps({
+			clientHeight,
+			clientWidth,
+      clientY,
+      clientX: parseFloat(window.getComputedStyle(draggedDOM.parentNode).paddingLeft)
+		});
+	};
+
+	// Normally you would want to split things out into separate components.
+	// But in this example everything is just done in one place for simplicity
+	return (
+		<DragDropContext onDragEnd={onDragEnd} onDragUpdate={onDragUpdate}>
+			<Droppable droppableId="droppable">
+				{(provided, snapshot) => (
+					<div
+						{...provided.droppableProps}
+						ref={provided.innerRef}
+						style={getListStyle(snapshot.isDraggingOver)}
+					>
+						{items.map((item, index) => (
+							<Draggable key={item.id} draggableId={item.id} index={index}>
+								{(provided, snapshot) => (
+									<div
+										ref={provided.innerRef}
+										{...provided.draggableProps}
+										{...provided.dragHandleProps}
+										style={getItemStyle(
+											snapshot.isDragging,
+											provided.draggableProps.style
+										)}
+									>
+										{item.content}
+									</div>
+								)}
+							</Draggable>
+						))}
+
+						{provided.placeholder}
+            {/* <CustomPlaceholder snapshot={snapshot} /> */}
+            <div style={{
+              position: "absolute",
+              top: placeholderProps.clientY,
+              left: placeholderProps.clientX,
+              height: placeholderProps.clientHeight,
+              background: "tomato",
+              width: placeholderProps.clientWidth
+            }}/>
+					</div>
+				)}
+			</Droppable>
+		</DragDropContext>
+	);
+};
 
 // Put the thing into the DOM!
 ReactDOM.render(<App />, document.getElementById("root"));
